bb129d5669051fd3929f71d2bea8a14fe4788465
devendra0809 <devendersk25@gmail.com>
2025-04-19 20:31:59 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "69"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+// "33" => 33
+// "33ab" => NaN (not a number)
+// "undefined" => NaN
+// "null" => 0
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true , 0 => false
+// "" => false
+// "devender" => true
+
