0e02279378b88994a160cf4d47091ec2a2f8a331
devendra0809 <devendersk25@gmail.com>
2025-04-19 21:02:52 +0000

    comparisons and conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,3 +19,11 @@ console.log(booleanIsLoggedIn)
 // "" => false
 // "devender" => true
 
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32 
+
+console.log(true) // true
+console.log(+true) // 1
+console.log(+"") // 0
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+console.log("2" > 1); // true
+// JS convert the string into a number
+console.log("02" > 1); // true
+
+console.log(null > 0); // false
+// null is treated as 0
+console.log(null == 0); // false
+// == has different working functionality from > and others
+console.log(null >= 0); // true
+// null is treated as 0
+
+
+
+
+
+
