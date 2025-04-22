c10fe855e33eca3e658855dd842fc3a0b01add49
devendra0809 <devendersk25@gmail.com>
2025-04-22 22:40:34 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,70 @@
+/* two types to decalre an object
+1. as literal (that is not singleton)
+2. as constructor (it is singleton)  (object.create)
+SINGLETON => it is an unique object 
+*/
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name : "Devender",
+    "full name" : "Devender Choudhary",
+    [mySym] : "myKey1",
+    age : 18,
+    location : "Jaipur",
+    email : "devender@google.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+// two ways to access a property from the object
+console.log(JsUser.email);
+// devender@google.com
+console.log(JsUser["email"]);
+// devender@google.com
+
+console.log(JsUser["full name"]);
+// Devender Choudhary
+// access of full name cannot be done by the dot operator
+
+console.log(JsUser[mySym]);
+// myKey1
+
+JsUser.email = "devender@chatgpt.com"
+
+//Object.freeze(JsUser)
+// to avoid or ignore any further changes in the object properties
+
+JsUser.email = "devender@microsoft.com"
+
+console.log(JsUser);
+/* {
+  name: 'Devender',
+  'full name': 'Devender Choudhary',
+  age: 18,
+  location: 'Jaipur',
+  email: 'devender@chatgpt.com',
+  isLoggedIn: false,
+  lastLoginDays: [ 'Monday', 'Saturday' ],
+  [Symbol(key1)]: 'myKey1'
+}
+*/
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");  
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);   
+}
+
+console.log(JsUser.greeting());
+// Hello JS User
+console.log(JsUser.greetingTwo());
+// Hello JS User, Devender
+
+
+
+
+
+
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,71 @@
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+// { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+const regularUser = {
+    email : "some@google.com",
+    fullname : {
+        userfullname : {
+            firstname : "Devender",
+            lastname : "Choudhary"
+        }
+    },
+    isLoggedIn : true
+}
+
+console.log(regularUser.fullname);
+// { userfullname: { firstname: 'Devender', lastname: 'Choudhary' } }
+
+console.log(regularUser.fullname.userfullname);
+// { firstname: 'Devender', lastname: 'Choudhary' }
+
+console.log(regularUser.fullname.userfullname.firstname);
+// Devender
+
+const obj1 = {
+    1 : "a",
+    2 : "b"
+}
+
+const obj2 = {
+    3 : "a",
+    4 : "b"
+}
+
+//const obj3 = Object.assign({}, obj1, obj2)
+// adding an empty {} obejct is a good practice
+// this means that all objects combines in the empty object
+// target obj => {}
+
+//console.log(obj3);
+// { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+// { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+// [ 'id', 'name', 'isLoggedIn' ]
+
+console.log(Object.values(tinderUser));
+// [ '123abc', 'Sammy', false ]
+
+console.log(Object.entries(tinderUser));
+// [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// true
+
+
+
+
+
+
+
