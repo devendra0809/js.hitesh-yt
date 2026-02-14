da7a45de94ca3e81577b59b9506c6e7eabc4d376
devendra0809 <devendersk25@gmail.com>
2026-02-14 12:34:33 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,14 @@
 const accountId = 144553
-let accountEmail = "devender@googe.com"
+let accountEmail = "devender@google.com"
 var accountPassword = "080922"
 accountCity = "Jaipur" // not a  safe option to declare a variable but it works in the javascript
-
+let accountState;
 
 //accountId = 2 //Not allowed as variable type is const
 
 /*
 prefer not to use var
-because of issue in block scope ans functional scope
+because of issue in block scope and functional scope
 */
 
 accountPassword = "080923"
@@ -16,4 +16,4 @@ accountCity = "Noida"
 accountEmail = "deepak@gmail.com"
 
 console.log(accountPassword);
-console.table([accountEmail, accountId, accountPassword])
+console.table([accountEmail, accountId, accountPassword, accountState])
